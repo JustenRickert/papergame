63796b903c106f47cee1949178905d8250f794d1
justenrickert <justenrickert@gmail.com>
2016-11-23 17:21:15 -0600

    got collision to work. They still pushing, though.

--- main.js
+++ main.js
@@ -6,14 +6,14 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /* Okay, so let there be a game board. It is to be shaped according to the
- * number of pieces that will be on the board. There will be two factions of
- * pieces which will correspond to the red side and the blue side. At first, the
- * game will be about having pieces that are good for fighting, then as you go
- * on economic pieces will be added, which will then allow upgrading units, and
- * even constructing new units from a factory. The game will be played by
- * assigning intelligence to the particular pieces---either to defend other
- * specific pieces, or attack unrelentingly---so that the end goal is one team
- * winning over the other. */
+ * number of pieces that will be on the board or something. There will be two
+ * factions of pieces which will correspond to the red side and the blue side.
+ * At first, the game will be about having pieces that are good for fighting,
+ * then as you go on economic pieces will be added, which will then allow
+ * upgrading units, and even constructing new units from a factory. The game
+ * will be played by assigning intelligence to the particular pieces---either to
+ * defend other specific pieces, or attack unrelentingly---so that the end goal
+ * is one team winning over the other. */
 var Vector = (function () {
     function Vector(x, y) {
         this.x = x;
@@ -74,11 +74,35 @@ var State = (function () {
     };
     return State;
 }());
+var Force = (function () {
+    function Force(mass, vect) {
+        this.mass = mass;
+        this.vect = vect;
+    }
+    return Force;
+}());
+var Momentum = (function () {
+    function Momentum(mass, vect) {
+        this.mass = mass;
+        this.vect = vect;
+    }
+    Momentum.prototype.vector = function () {
+        return new Vector(this.mass * this.vect.x, this.mass * this.vect.y);
+    };
+    return Momentum;
+}());
+var Physics = (function () {
+    function Physics(force, momen) {
+        this.force = force;
+        this.momen = momen;
+    }
+    return Physics;
+}());
 // Circles are cool!
 var Circle = (function () {
-    function Circle(radius, pos, vel, acc, color, bandColor, direction, speed, acc_value, turnRate, state) {
+    function Circle(radius, pos, vel, phys, color, bandColor, direction, speed, acc_value, turnRate, state) {
         if (vel === void 0) { vel = new Vector(0, 0); }
-        if (acc === void 0) { acc = new Vector(0, 0); }
+        if (phys === void 0) { phys = new Physics(new Force(Math.PI * radius, new Vector(0, 0)), new Momentum(Math.PI * radius, new Vector(0, 0))); }
         if (color === void 0) { color = 'Black'; }
         if (bandColor === void 0) { bandColor = 'Black'; }
         if (direction === void 0) { direction = 0; }
@@ -89,7 +113,7 @@ var Circle = (function () {
         this.radius = radius;
         this.pos = pos;
         this.vel = vel;
-        this.acc = acc;
+        this.phys = phys;
         this.color = color;
         this.bandColor = bandColor;
         this.direction = direction;
@@ -104,8 +128,20 @@ var Circle = (function () {
     Circle.prototype.moveForwardByVel = function () {
         this.pos = new Vector(this.pos.x + this.speed * this.vel.x, this.pos.y + this.speed * this.vel.y);
     };
+    Circle.prototype.moveForwardByVec = function (vec) {
+        this.pos = Vector.plus(this.pos, vec);
+    };
+    // public moveVelByAcc(): void {
+    //     if (this.acc.x === 0 && this.acc.y === 0) {
+    //         this.vel = new Vector(
+    //             this.vel.x + this.acc_value * this.acc.x,
+    //             this.vel.y + this.acc_value * this.acc.y);
+    //     } else {
+    //         this.vel = new Vector(0, 0);
+    //     }
+    // }
     Circle.prototype.adjustVelocityToDirection = function () {
-        this.setVelocity(new Vector(Math.sin(this.direction), Math.cos(this.direction)));
+        this.setVel(new Vector(Math.sin(this.direction), Math.cos(this.direction)));
     };
     Circle.prototype.turn = function (delta) {
         /* a positive value indicates turning clockwise,  */
@@ -114,17 +150,17 @@ var Circle = (function () {
         this.adjustVelocityToDirection();
     };
     Circle.prototype.turnToPosition = function (pos) {
-        if (Math.abs(Vector.angleBetween(this.vel, Vector.minus(pos, this.pos))) > .07) {
+        if (Math.abs(Vector.angleBetween(this.vel, Vector.minus(pos, this.pos))) > 0.07) {
             this.turn(this.turnRate * Vector.directionTo(this.vel, Vector.minus(pos, this.pos)));
         }
     };
     Circle.prototype.setSpeed = function (spd) {
         this.speed = spd;
     };
-    Circle.prototype.setVelocity = function (vel) {
+    Circle.prototype.setVel = function (vel) {
         this.vel = new Vector(vel.x, vel.y);
     };
-    Circle.prototype.addVelocity = function (vel) {
+    Circle.prototype.addVel = function (vel) {
         this.vel = new Vector(this.vel.x + vel.x, this.vel.y + vel.y);
     };
     Circle.prototype.moveToPosition = function (pos) {
@@ -160,6 +196,9 @@ var Circle = (function () {
     };
     Circle.prototype.unsetColliding = function () {
         this.state.colliding = false;
+        this.phys.force.vect = new Vector(0, 0);
+    };
+    Circle.prototype.moveMomentumVector = function () {
     };
     Circle.isThenColliding = function (c1, c2) {
         if (Circle.isColliding(c1, c2)) {
@@ -171,8 +210,28 @@ var Circle = (function () {
             c2.unsetColliding();
         }
     };
+    // These static methods need to be in Circle, and not Vector, because they
+    // need access to Circle.radius and other attributes.
     Circle.isColliding = function (c1, c2) {
         return Vector.dist(c1.pos, c2.pos) < c1.radius + c2.radius;
+    };
+    Circle.applyForce = function (c1, c2) {
+        if (this.isColliding(c1, c2)) {
+            var diff = Vector.dist(c1.pos, c2.pos) - c1.radius - c2.radius;
+            var dirTo = Vector.minus(c1.pos, c2.pos);
+            c1.phys.force.vect = Vector.times(-diff, dirTo);
+            c2.phys.force.vect = Vector.times(diff, dirTo);
+        }
+    };
+    Circle.prototype.impulse = function () {
+        this.phys.momen.vect = Vector.plus(this.phys.momen.vect, this.phys.force.vect);
+    };
+    Circle.prototype.moveByMomentum = function () {
+        this.moveForwardByVec(Vector.times(1 / this.phys.momen.mass, this.phys.momen.vect));
+    };
+    Circle.prototype.frictionMomentum = function (coeff) {
+        /* coeff should be in (0,1) that's non-inclusive */
+        this.phys.momen.vect = Vector.times(coeff, this.phys.momen.vect);
     };
     return Circle;
 }());
@@ -210,14 +269,15 @@ CANV.height = 2380;
 var CNTX = CANV.getContext("2d");
 var red = new RedCircle();
 var blu = new BlueCircle();
-red.setVelocity(new Vector(1, 0));
-blu.setVelocity(new Vector(0.5, 0));
+red.setVel(new Vector(1, 0));
+blu.setVel(new Vector(0.5, 0));
 blu.setSpeed(0.5);
 var GAME_FRAME = 0;
 // I want this to be kind of a portable test service or something. I dunno,
 // maybe I'll make an elaborate test module or something, too.
 function start() {
     //Vector.minus(red.pos, new Vector(0, 0))))
+    console.log(red.phys.momen.vect);
     clearScreen();
     Circle.isThenColliding(red, blu);
     // console.log(red.state, blu.state);
@@ -229,10 +289,23 @@ function start() {
     if (red.state.colliding) {
         if (Math.abs(Vector.angleBetween(red.vel, Vector.minus(red.pos, blu.pos)))
             < Math.PI / 1.4) {
-            // circle move around the other circle. This is so far a naive way of dealing
-            // with this, but it should be too hard to add a physics based "pushing"
-            // affect simply on top of this.
+            // circle move around the other circle. This is so far a naive way
+            // of dealing with this, but it shouldn't be too hard to add a
+            // physics-based "pushing" affect simply on top of this. I am trying
+            // to make a point of it because I think it should be easily seen,
+            // as I don't know if I want to make it part of the Circle itself
+            // just yet (and I don't want to just forget where this number is
+            // located).
             red.moveToPosition(LASTCLICK);
+            // In order to abstract these physics functions, we need a higher
+            // order way of talking about the individual units, so that if there
+            // is more than one collision happening, we can account for _all_ of
+            // those interactions
+            Circle.applyForce(red, blu);
+            red.impulse(); // impulse is Force per time.
+            blu.impulse(); // So this thing is starting to move more.
+            red.moveByMomentum(); // This actually moves the point.
+            blu.moveByMomentum();
         }
         else {
             red.turnToPosition(LASTCLICK);
@@ -240,6 +313,12 @@ function start() {
     }
     else {
         red.moveToPosition(LASTCLICK);
+        red.unsetColliding(); // This is for the state
+        blu.unsetColliding(); // Really, it should be blu.state.unsetColliding() {I think?}
+        red.frictionMomentum(0.9); // Slows the thing down afterwards
+        blu.frictionMomentum(0.9);
+        red.moveByMomentum(); // Still needs to move, even if not colliding
+        blu.moveByMomentum();
     }
     red.draw();
     blu.draw();
@@ -247,6 +326,13 @@ function start() {
     requestAnimationFrame(start);
 }
 start();
+// I don't use what is below this comment anymore, but I may be able generalize
+// them, and then use them to draw everything.
+// ALSO, be keeping these intensive drawing operations outside of classes, they
+// can be moved to their own separate files wherein one can be certain the
+// values have an immutable state, then the drawing operations can be called
+// concurrently to increase the performance (I think? Maybe it won't matter).
+// URL: https://developer.mozilla.org/en-US/docs/Web/JavaScript/EventLoop
 // clears the screen, obvii
 function clearScreen() {
     CNTX.clearRect(0, 0, CANV.width, CANV.height);
--- main.tsx
+++ main.tsx
@@ -2,14 +2,14 @@
 // NEW PROJECT WOOH
 
 /* Okay, so let there be a game board. It is to be shaped according to the
- * number of pieces that will be on the board. There will be two factions of
- * pieces which will correspond to the red side and the blue side. At first, the
- * game will be about having pieces that are good for fighting, then as you go
- * on economic pieces will be added, which will then allow upgrading units, and
- * even constructing new units from a factory. The game will be played by
- * assigning intelligence to the particular pieces---either to defend other
- * specific pieces, or attack unrelentingly---so that the end goal is one team
- * winning over the other. */
+ * number of pieces that will be on the board or something. There will be two
+ * factions of pieces which will correspond to the red side and the blue side.
+ * At first, the game will be about having pieces that are good for fighting,
+ * then as you go on economic pieces will be added, which will then allow
+ * upgrading units, and even constructing new units from a factory. The game
+ * will be played by assigning intelligence to the particular pieces---either to
+ * defend other specific pieces, or attack unrelentingly---so that the end goal
+ * is one team winning over the other. */
 
 class Vector {
     constructor(
@@ -58,6 +58,7 @@ class Vector {
         }
     }
 }
+
 class State {
     constructor() { }
     public movement = true;
@@ -69,13 +70,38 @@ class State {
     }
 }
 
+class Force {
+    constructor(
+        public mass: number,
+        public vect: Vector) { }
+}
+
+class Momentum {
+    constructor(
+        public mass: number,
+        public vect: Vector) { }
+    public vector(): Vector {
+        return new Vector(
+            this.mass * this.vect.x,
+            this.mass * this.vect.y);
+    }
+}
+
+class Physics {
+    constructor(
+        public force: Force,
+        public momen: Momentum) { }
+}
+
 // Circles are cool!
 class Circle {
     constructor(
         public radius: number,
         public pos: Vector,
         public vel: Vector = new Vector(0, 0),
-        public acc: Vector = new Vector(0, 0),
+        public phys: Physics = new Physics(
+            new Force(Math.PI * radius, new Vector(0, 0)),
+            new Momentum(Math.PI * radius, new Vector(0, 0))),
         public color: string = 'Black',
         public bandColor: string = 'Black',
         public direction: number = 0,
@@ -92,8 +118,20 @@ class Circle {
             this.pos.x + this.speed * this.vel.x,
             this.pos.y + this.speed * this.vel.y);
     }
+    public moveForwardByVec(vec: Vector): void {
+        this.pos = Vector.plus(this.pos, vec);
+    }
+    // public moveVelByAcc(): void {
+    //     if (this.acc.x === 0 && this.acc.y === 0) {
+    //         this.vel = new Vector(
+    //             this.vel.x + this.acc_value * this.acc.x,
+    //             this.vel.y + this.acc_value * this.acc.y);
+    //     } else {
+    //         this.vel = new Vector(0, 0);
+    //     }
+    // }
     private adjustVelocityToDirection(): void {
-        this.setVelocity(new Vector(Math.sin(this.direction), Math.cos(this.direction)));
+        this.setVel(new Vector(Math.sin(this.direction), Math.cos(this.direction)));
     }
     public turn(delta): void {
         /* a positive value indicates turning clockwise,  */
@@ -102,17 +140,17 @@ class Circle {
         this.adjustVelocityToDirection();
     }
     public turnToPosition(pos: Vector): void {
-        if (Math.abs(Vector.angleBetween(this.vel, Vector.minus(pos, this.pos))) > .07) {
+        if (Math.abs(Vector.angleBetween(this.vel, Vector.minus(pos, this.pos))) > 0.07) { // 0.07 could maybe be abstracted
             this.turn(this.turnRate * Vector.directionTo(this.vel, Vector.minus(pos, this.pos)));
         }
     }
     public setSpeed(spd: number): void {
         this.speed = spd;
     }
-    public setVelocity(vel: Vector): void {
+    public setVel(vel: Vector): void {
         this.vel = new Vector(vel.x, vel.y);
     }
-    public addVelocity(vel: Vector): void {
+    public addVel(vel: Vector): void {
         this.vel = new Vector(this.vel.x + vel.x, this.vel.y + vel.y);
     }
     public moveToPosition(pos: Vector): void {
@@ -154,6 +192,9 @@ class Circle {
     }
     public unsetColliding() {
         this.state.colliding = false;
+        this.phys.force.vect = new Vector(0, 0);
+    }
+    public moveMomentumVector() {
     }
     static isThenColliding(c1: Circle, c2: Circle): void {
         if (Circle.isColliding(c1, c2)) {
@@ -164,8 +205,29 @@ class Circle {
             c2.unsetColliding();
         }
     }
+    // These static methods need to be in Circle, and not Vector, because they
+    // need access to Circle.radius and other attributes.
     static isColliding(c1: Circle, c2: Circle): boolean {
-        return Vector.dist(c1.pos, c2.pos) < c1.radius + c2.radius
+        return Vector.dist(c1.pos, c2.pos) < c1.radius + c2.radius;
+    }
+    static applyForce(c1: Circle, c2: Circle): void {
+        if (this.isColliding(c1, c2)) {
+            var diff = Vector.dist(c1.pos, c2.pos) - c1.radius - c2.radius;
+            var dirTo = Vector.minus(c1.pos, c2.pos);
+            c1.phys.force.vect = Vector.times(-diff, dirTo);
+            c2.phys.force.vect = Vector.times(diff, dirTo);
+        }
+    }
+    public impulse(): void {
+        this.phys.momen.vect = Vector.plus(this.phys.momen.vect, this.phys.force.vect)
+    }
+    public moveByMomentum(): void {
+        this.moveForwardByVec(
+            Vector.times(1 / this.phys.momen.mass, this.phys.momen.vect));
+    }
+    public frictionMomentum(coeff: number): void {
+        /* coeff should be in (0,1) that's non-inclusive */
+        this.phys.momen.vect = Vector.times(coeff, this.phys.momen.vect);
     }
 }
 
@@ -204,8 +266,8 @@ var CNTX = CANV.getContext("2d");
 
 var red = new RedCircle();
 var blu = new BlueCircle();
-red.setVelocity(new Vector(1, 0));
-blu.setVelocity(new Vector(0.5, 0));
+red.setVel(new Vector(1, 0));
+blu.setVel(new Vector(0.5, 0));
 blu.setSpeed(0.5);
 var GAME_FRAME = 0;
 
@@ -213,6 +275,7 @@ var GAME_FRAME = 0;
 // maybe I'll make an elaborate test module or something, too.
 function start() {
     //Vector.minus(red.pos, new Vector(0, 0))))
+    console.log(red.phys.momen.vect);
     clearScreen();
     Circle.isThenColliding(red, blu);
     // console.log(red.state, blu.state);
@@ -222,16 +285,42 @@ function start() {
     }
     if (red.state.colliding) {
         if (Math.abs(Vector.angleBetween(red.vel, Vector.minus(red.pos, blu.pos)))
-            < Math.PI / 1.4) {// 1.4 is a kind of "squeeze" amount. It let's the one
-            // circle move around the other circle. This is so far a naive way of dealing
-            // with this, but it should be too hard to add a physics based "pushing"
-            // affect simply on top of this.
+            < Math.PI / 1.4) {// 1.4 is a kind of "squeeze" amount. It lets the one
+            // circle move around the other circle. This is so far a naive way
+            // of dealing with this, but it shouldn't be too hard to add a
+            // physics-based "pushing" affect simply on top of this. I am trying
+            // to make a point of it because I think it should be easily seen,
+            // as I don't know if I want to make it part of the Circle itself
+            // just yet (and I don't want to just forget where this number is
+            // located).
             red.moveToPosition(LASTCLICK);
+
+            // In order to abstract these physics functions, we need a higher
+            // order way of talking about the individual units, so that if there
+            // is more than one collision happening, we can account for _all_ of
+            // those interactions
+            Circle.applyForce(red, blu);
+            red.impulse();      // impulse is Force per time.
+            blu.impulse();      // So this thing is starting to move more.
+            red.moveByMomentum(); // This actually moves the point.
+            blu.moveByMomentum();
+            // The next step is to make it so that if the unit detects that it
+            // is not moving, it will attempt to push itself past what is in
+            // front of it. At that point, I think that the basic
+            // physics/collision system will be complete. We'll see what will be
+            // needed in the future as we start considering exactly how the
+            // pieces are to interact.
         } else {
             red.turnToPosition(LASTCLICK);
         }
     } else {
         red.moveToPosition(LASTCLICK);
+        red.unsetColliding();   // This is for the state
+        blu.unsetColliding();   // Really, it should be blu.state.unsetColliding() {I think?}
+        red.frictionMomentum(0.9); // Slows the thing down afterwards
+        blu.frictionMomentum(0.9);
+        red.moveByMomentum();   // Still needs to move, even if not colliding
+        blu.moveByMomentum();
     }
 
     red.draw();
@@ -239,6 +328,15 @@ function start() {
     GAME_FRAME++;
     requestAnimationFrame(start);
 } start()
+
+// I don't use what is below this comment anymore, but I may be able generalize
+// them, and then use them to draw everything.
+
+// ALSO, be keeping these intensive drawing operations outside of classes, they
+// can be moved to their own separate files wherein one can be certain the
+// values have an immutable state, then the drawing operations can be called
+// concurrently to increase the performance (I think? Maybe it won't matter).
+// URL: https://developer.mozilla.org/en-US/docs/Web/JavaScript/EventLoop
 
 // clears the screen, obvii
 function clearScreen() {
